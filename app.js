18de82b8882848d21b92c058fa971c031a9bd376
Wendy <wendyyang@developersinstitute.co.nz>
2021-11-08 18:34:32 +1300

    add event listener for changing bg color

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const button = document.getElementById("change-bg-button");
+const color = document.querySelector(".color");
+
+button.addEventListener("click", function () {
+  console.log(document.body);
+  document.body.style.background = "red";
+});
