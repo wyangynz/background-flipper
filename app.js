9c75f2e52a16bcf3361ddf5f12615636da5257ba
Wendy <wendyyang@developersinstitute.co.nz>
2021-11-08 19:15:58 +1300

    remove commented out

--- app.js
+++ app.js
@@ -1,34 +1,12 @@
-// const colorArray = [
-//   "thistle",
-//   "cornflowerblue",
-//   "mintcream",
-//   "mistyrose",
-//   "honeydew",
-//   "peachpuff",
-//   "papayawhip",
-//   "antiquewhite",
-//   "lavender",
-//   "tomato",
-//   "lightcoral",
-// ];
 const button = document.getElementById("change-bg-button");
 const colorText = document.querySelector(".color-text");
 
 button.addEventListener("click", function () {
-  //   const randomNumber = getRandomNumber();
-  //   console.log(randomNumber);
-  //   console.log(colorArray[randomNumber]);
-  //   document.body.style.background = colorArray[randomNumber];
-  //   colorText.textContent = colorArray[randomNumber];
   const randomHexCode = getRandomHexCode();
   console.log(randomHexCode);
   document.body.style.background = randomHexCode;
   colorText.textContent = randomHexCode;
 });
-
-const getRandomNumber = () => {
-  return Math.floor(Math.random() * colorArray.length);
-};
 
 const getRandomHexCode = () => {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
