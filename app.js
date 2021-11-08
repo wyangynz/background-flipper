c761acb4af9d6cb27c499f667469b8f5859e181c
Wendy <wendyyang@developersinstitute.co.nz>
2021-11-08 19:00:41 +1300

    add colors to colorArray

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const colorArray = ["thistle", "cornflowerblue", "mintgreen", "mistyrose"];
+const colorArray = [
+  "thistle",
+  "cornflowerblue",
+  "mintcream",
+  "mistyrose",
+  "honeydew",
+  "peachpuff",
+  "papayawhip",
+  "antiquewhite",
+  "lavender",
+  "tomato",
+  "lightcoral",
+];
 const button = document.getElementById("change-bg-button");
 const colorText = document.querySelector(".color-text");
 
 button.addEventListener("click", function () {
-  //   console.log(document.body);
-  // const randomNumber = getRandomNumber();
-  const randomNumber = 0;
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+  console.log(colorArray[randomNumber]);
   document.body.style.background = colorArray[randomNumber];
   colorText.textContent = colorArray[randomNumber];
 });
 
 const getRandomNumber = () => {
-  return Math.random();
+  return Math.floor(Math.random() * colorArray.length);
 };
