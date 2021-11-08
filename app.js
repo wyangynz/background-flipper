465ad6148665f687856dee5cd78993110012e24a
Wendy <wendyyang@developersinstitute.co.nz>
2021-11-08 19:11:45 +1300

    add randomhexCode

--- app.js
+++ app.js
@@ -15,13 +15,21 @@ const button = document.getElementById("change-bg-button");
 const colorText = document.querySelector(".color-text");
 
 button.addEventListener("click", function () {
-  const randomNumber = getRandomNumber();
-  console.log(randomNumber);
-  console.log(colorArray[randomNumber]);
-  document.body.style.background = colorArray[randomNumber];
-  colorText.textContent = colorArray[randomNumber];
+  //   const randomNumber = getRandomNumber();
+  //   console.log(randomNumber);
+  //   console.log(colorArray[randomNumber]);
+  //   document.body.style.background = colorArray[randomNumber];
+  //   colorText.textContent = colorArray[randomNumber];
+  const randomHexCode = getRandomHexCode();
+  console.log(randomHexCode);
+  document.body.style.background = randomHexCode;
+  colorText.textContent = randomHexCode;
 });
 
 const getRandomNumber = () => {
   return Math.floor(Math.random() * colorArray.length);
+};
+
+const getRandomHexCode = () => {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
 };
